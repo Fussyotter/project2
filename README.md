8eb1927f660b9dc844d703b309e12f15fb59d8b7
ialoysious <isaacaloysious@gmail.com>
2023-02-23 16:16:20 -0800

    readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# README.md
+
+## Fish!
+
+We are creating a CRUD App so that users can view the data hosted by http://www.fishwatch.gov. Our app will be an easy to view way of browsing through this collected information with an emphasis on lighter information with the functionality to add and edit details about fish.
+
+## Technologies
+
+Javascript
+Mongoose
+Express
--- models/fishSchema.js
+++ models/fishSchema.js
@@ -2,17 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const fishSchema = new Schema({
+    // 4-5 information types
 	Habitat: String,
-	'Image Gallery': [
-		{
-			src: String,
-		},
-	],
+	'Image Gallery': String,
 	Location: String,
-	'Noaa Fisheries Region': String,
+	// 'Noaa Fisheries Region': String,
 	Population: String,
-	'Population Status': String,
-	'Scientific Name': String,
+	// 'Population Status': String,
+	// 'Scientific Name': String,
 	'Species Aliases': String,
-	'Species Illustration Photo': [],
+    taste: String,
+    quote: String,
+    'Health benefits': String,
+	'Species Illustration Photo': [
+        {
+            src: String,
+        }
+    ],
 });
