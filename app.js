54018d1a9b29f8cb4d683fc934a81128a5532bc3
dominicmena <mena.dominic@gmail.com>
2023-02-27 11:53:32 -0600

    test:

--- app.js
+++ app.js
@@ -15,19 +15,20 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-app.get('/fish', (request, response) => {
-	fishSchema.find((error, allFish) => {
+app.get('/fish', (request, response)=> {
+	fishSchema.find( (error,allFish) => {
 		response.render('index.ejs', {
-			fish: allFish,
-		});
-	});
-});
+			fish : allFish 
+		})
+	})
+})
 
 // Temporary
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB, () => {
 	console.log('The connection with mongod is established');
 });
+
 
 app.get('/fish/seed', (req, res) => {
 	fishSchema.create(fishArr, (error, seedData) => {
@@ -39,18 +40,18 @@ app.get('/fish/seed', (req, res) => {
 });
 
 app.get('/fish', (req, res) => {
-	fishSchema.find({}, (error, allFish) => {
-		res.render('index.ejs', { fish: allFish });
+	recipeSchema.find({}, (error, allFish) => {
+		res.render('index.ejs', { fishArr: allFish });
 	});
 });
 
-app.get('/fish/:id', (request, response) => {
+app.get('/fish/:id', (request, response)=> {
 	fishSchema.findById(request.params.id, (err, foundFish) => {
 		response.render('show.ejs', {
-			fishArr: foundFish,
-		});
-	});
-});
+			fishArr : foundFish
+		})
+	})
+})
 
 // app.get('/new', (request,response) {
 // 	response.render('new.ejs')
@@ -62,30 +63,25 @@ app.get('/fish/:id', (request, response) => {
 // 	})
 // })
 
-app.delete('/fish/:id', (request, response) => {
-	fishSchema.findByIdAndRemove(request.params.id, (error, fishDelete) => {
-		response.redirect('/fish');
-	});
-});
+app.delete('/fish/:id', (request,response)=> {
+	fishSchema.findByIdAndRemove(request.params.id, (error, fishDelete)=> {
+		response.redirect('/fish')
+	})
+})
 
-app.get('fish/:id/edit', (request, response) => {
-	fishSchema.findById(request.params.id, (err, fishEdit) => {
+app.get('fish/:id/edit', (request,response)=> {
+	fishSchema.findById(request.params.id, (err, fishEdit)=> {
 		response.render('edit.ejs', {
-			fishArr: fishEdit,
-		});
-	});
-});
+			fishArr : fishEdit
+		})
+	})
+})
 
-app.put('/fish/:id', (request, response) => {
-	fishSchema.findByIdAndUpdate(
-		request.params.id,
-		request.body,
-		{ new: true },
-		(err, fishEdit) => {
-			response.redirect('/fish');
-		}
-	);
-});
+app.put('/fish/:id', (request, response)=> {
+	fishSchema.findByIdAndUpdate(request.params.id, request.body, {new:true}, (err, fishEdit) => {
+		response.redirect('/fish')
+	})
+})
 
 app.listen(3000, () => {
 	console.log('listening');
