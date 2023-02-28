42f43df6e15a1c050fcab3db0b0cebe10588820f
ialoysious <isaacaloysious@gmail.com>
2023-02-28 12:41:04 -0800

    search working

--- models/search.js
+++ models/search.js
@@ -1,13 +1,15 @@
+// redundant
+
 // Added load so that the page has time to finish loading before it runs.
 
 window.addEventListener('load', () => {
 	const searchButton = document.getElementById('searchButton');
 	const searchInput = document.getElementById('searchInput');
-// this is where the issue seems to be.  I can't get the searchValue to move into it
+	// this is where the issue seems to be.  I can't get the searchValue to move into it
 	searchButton.addEventListener('click', async () => {
 		const searchValue = searchInput.value;
 		const url = `/search?fish=${encodeURIComponent(searchValue)}`;
 		window.location.href = url;
-        console.log(url);
+		console.log(url);
 	});
 });
